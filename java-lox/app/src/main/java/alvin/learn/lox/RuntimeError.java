ea2491507eb0d84c3580d1cdf967049a47714c4d
alvin huang <huanguolin@outlook.com>
2022-04-10 23:33:28 +0800

    add Interpreter

---
+++ java-lox/app/src/main/java/alvin/learn/lox/RuntimeError.java
@@ -0,0 +1,10 @@
+package alvin.learn.lox;
+
+class RuntimeError extends RuntimeException {
+  final Token token;
+
+  RuntimeError(Token token, String message) {
+    super(message);
+    this.token = token;
+  }
+}
